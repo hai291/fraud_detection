bad27e7c0417392fb31a89687d34f89be724bf23
hai291 <11247282@st.neu.edu.vn>
2025-09-28 18:05:59 +0700

    Delete src/models directory

--- src/models/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Models module for neural network architectures
-"""
-
-from .base_model import *
-from .transformer_model import *
-from .cnn_model import *
-
-__all__ = ['BaseModel', 'TransformerModel', 'CNNModel']
